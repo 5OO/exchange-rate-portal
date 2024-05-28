736d9eed79d1a59c7b60aa75ca7a1ef82789613d
5OO <git.hub@myrk.eu>
2024-05-28 17:07:18 +0300

    Initial commit - project setup

---
+++ src/main/java/org/sberp/exchangerateportal/ExchangeRatePortalApplication.java
@@ -0,0 +1,13 @@
+package org.sberp.exchangerateportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeRatePortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExchangeRatePortalApplication.class, args);
+    }
+
+}
