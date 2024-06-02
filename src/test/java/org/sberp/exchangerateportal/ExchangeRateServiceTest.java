5894d8d987757a7369fc66bdc3bfa968c1c62b0c
5OO <git.hub@myrk.eu>
2024-06-03 00:27:26 +0300

    Update ExchangeRateServiceTest to handle latest rates
    Updated the test case in ExchangeRateServiceTest to handle the retrieval of latest exchange rates. This includes introducing a new mock for CurrencyNameRepository and altering the tested method to make use of said repository.

--- src/test/java/org/sberp/exchangerateportal/ExchangeRateServiceTest.java
+++ src/test/java/org/sberp/exchangerateportal/ExchangeRateServiceTest.java
@@ -5,12 +5,19 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.sberp.exchangerateportal.dto.ExchangeRateDTO;
+import org.sberp.exchangerateportal.model.CurrencyName;
 import org.sberp.exchangerateportal.model.ExchangeRate;
+import org.sberp.exchangerateportal.repository.CurrencyNameRepository;
 import org.sberp.exchangerateportal.repository.ExchangeRateRepository;
 import org.sberp.exchangerateportal.service.ExchangeRateService;
 
+import java.time.LocalDate;
 import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -23,16 +30,27 @@ class ExchangeRateServiceTest {
     @Mock
     private ExchangeRateRepository exchangeRateRepository;
 
+    @Mock
+    private CurrencyNameRepository currencyNameRepository;
 
     @Test
-    void testGetAllRates() {
+    void testGetLatestRates() {
         ExchangeRate rate = new ExchangeRate();
         rate.setCurrency("USD");
         rate.setRate(1.1);
-        rate.setDate("2024-05-24");
+        rate.setDate(String.valueOf(LocalDate.of(2024, 5, 24)));
 
-        when(exchangeRateRepository.findAll()).thenReturn(Collections.singletonList(rate));
+        CurrencyName currencyName = new CurrencyName("USD", "US Dollar", "United States");
 
+        when(exchangeRateRepository.findLatestRates()).thenReturn(Collections.singletonList(rate));
+        when(currencyNameRepository.findById("USD")).thenReturn(Optional.of(currencyName));
+
+        List<ExchangeRateDTO> latestRates = exchangeRateService.getLatestRates();
+        assertEquals(1, latestRates.size());
+        assertEquals("USD", latestRates.get(0).getCurrency());
+        assertEquals(1.1, latestRates.get(0).getRate());
+        assertEquals("US Dollar", latestRates.get(0).getCurrencyName());
+        assertEquals("United States", latestRates.get(0).getEntityLocation());
     }
 
     @Test
