2d72293e3e240e569746b6b89c07e624c932b0fb
5OO <git.hub@myrk.eu>
2024-06-05 07:35:43 +0300

    Update test parameters in ExchangeRateServiceTest
    The test parameters in the ExchangeRateServiceTest have been revised. A RestTemplate mock is added and a lenient expectation is now set for it to give a mocked XML response for the getForObject method. The rate values in getLatestRates and testGetHistoricalRatesByCurrency tests are also updated. Changes align tests with recent application functionality.

--- src/test/java/org/sberp/exchangerateportal/ExchangeRateServiceTest.java
+++ src/test/java/org/sberp/exchangerateportal/ExchangeRateServiceTest.java
@@ -12,6 +12,7 @@ import org.sberp.exchangerateportal.repository.CurrencyNameRepository;
 import org.sberp.exchangerateportal.repository.ExchangeRateRepository;
 import org.sberp.exchangerateportal.service.ExchangeRateService;
 import org.springframework.hateoas.PagedModel;
+import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDate;
 import java.util.Collections;
@@ -20,6 +21,9 @@ import java.util.Optional;
 import java.util.stream.StreamSupport;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -35,11 +39,14 @@ class ExchangeRateServiceTest {
     @Mock
     private CurrencyNameRepository currencyNameRepository;
 
+    @Mock
+    private RestTemplate restTemplate;
+
     @Test
     void testGetLatestRates() {
         ExchangeRate rate = new ExchangeRate();
         rate.setCurrency("USD");
-        rate.setRate(1.1);
+        rate.setRate(1.2854);
         rate.setDate(String.valueOf(LocalDate.of(2024, 5, 24)));
 
         CurrencyName currencyName = new CurrencyName("USD", "US Dollar", "United States");
@@ -50,31 +57,54 @@ class ExchangeRateServiceTest {
         List<ExchangeRateDTO> latestRates = exchangeRateService.getLatestRates();
         assertEquals(1, latestRates.size());
         assertEquals("USD", latestRates.get(0).getCurrency());
-        assertEquals(1.1, latestRates.get(0).getRate());
+        assertEquals(1.2854, latestRates.get(0).getRate());
         assertEquals("US Dollar", latestRates.get(0).getCurrencyName());
         assertEquals("United States", latestRates.get(0).getEntityLocation());
     }
 
     @Test
     void testGetHistoricalRatesByCurrency() {
-        ExchangeRate rate = new ExchangeRate();
-        rate.setCurrency("USD");
-        rate.setRate(1.0852);
-        rate.setDate(String.valueOf(LocalDate.of(2024, 5, 24)));
+        String mockResponse = """
+        <FxRates>
+            <FxRate>
+                <Tp>EU</Tp>
+                <Dt>2024-05-31</Dt>
+                <CcyAmt>
+                    <Ccy>USD</Ccy>
+                    <Amt>1.0852</Amt>
+                </CcyAmt>
+            </FxRate>
+            <FxRate>
+                <Tp>EU</Tp>
+                <Dt>2024-06-04</Dt>
+                <CcyAmt>
+                    <Ccy>USD</Ccy>
+                    <Amt>1.0865</Amt>
+                </CcyAmt>
+            </FxRate>
+        </FxRates>
+    """;
+
+        lenient().when(restTemplate.getForObject(anyString(), eq(String.class))).thenReturn(mockResponse);
 
         CurrencyName currencyName = new CurrencyName("USD", "US Dollar", "United States");
 
         when(currencyNameRepository.findById("USD")).thenReturn(Optional.of(currencyName));
 
-        PagedModel<ExchangeRateDTO> historicalRatesModel = exchangeRateService.getHistoricalRatesByCurrency("USD", 0, 10);
+        PagedModel<ExchangeRateDTO> historicalRatesModel = exchangeRateService.getHistoricalRatesByCurrency("USD", 0, 100);
 
         List<ExchangeRateDTO> historicalRates = StreamSupport.stream(historicalRatesModel.spliterator(), false)
                 .toList();
 
-        assertEquals(10, historicalRates.size());
-        assertEquals("USD", historicalRates.get(0).getCurrency());
-        assertEquals(1.0852, historicalRates.get(0).getRate());
-        assertEquals("US Dollar", historicalRates.get(0).getCurrencyName());
-        assertEquals("United States", historicalRates.get(0).getEntityLocation());
+        ExchangeRateDTO rateForSpecificDate = historicalRates.stream()
+                .filter(rate -> rate.getDate().equals("2024-05-31"))
+                .findFirst()
+                .orElse(null);
+
+        assert rateForSpecificDate != null;
+        assertEquals("USD", rateForSpecificDate.getCurrency());
+        assertEquals(1.0852, rateForSpecificDate.getRate());
+        assertEquals("US Dollar", rateForSpecificDate.getCurrencyName());
+        assertEquals("United States", rateForSpecificDate.getEntityLocation());
     }
 }
